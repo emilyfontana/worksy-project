e769a07b9d53746a5220e7c4ca9d0107de01c111
Emily Fontana <160985032+emilyfontana@users.noreply.github.com>
2026-06-10 23:13:26 -0300

    commit changes

--- backend/Config/db.js
+++ backend/Config/db.js
@@ -3,6 +3,6 @@ import mysql from "mysql2";
 export const db = mysql.createConnection({
     host:"localhost",
     user: "root",
-    password: "Epf100406!",
+    password: "",
     database: "worksy"
 });
