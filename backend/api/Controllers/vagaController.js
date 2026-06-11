42caeb87244d7339e134047bbbe2a04fca74b27a
Emilyfontana <emilypfontana@gmail.com>
2026-06-10 21:06:45 -0300

    insercao de parametros nos controllers

--- backend/api/Controllers/vagaController.js
+++ backend/api/Controllers/vagaController.js
@@ -1,22 +1,30 @@
 const getVagaID = async (req, res) => {
+    const { id } = req.params;
     res.json({
         message: "buscar vaga"
     });
 };
 
 const criarVaga = async (req, res) => {
+    const { titulo, descricao, salario, modalidade } = req.body;
+    const empresa_id = req.usuario.id;
     res.json ({
         message : "Criar vaga"
     });
 };
 
 const updateVaga = async (req, res) => {
+    const { id } = req.params;
+    const { titulo, descricao, salario, modalidade, status } = req.body;
+    const empresa_id = req.usuario.id;
     res.json ({
         message: "atualizar vaga"
     });
 };
 
 const deleteVaga = async (req, res) => {
+    const { id } = req.params;
+    const empresa_id = req.usuario.id;
     res.json ({
         message: "deleta vaga"
     
