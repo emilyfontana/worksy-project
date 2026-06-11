16cb06d587f2e26538f3066662f542f35ce2b4a2
Emily <emilypfontana@gmail.com>
2026-06-10 23:14:17 -0300

    env

--- backend/Config/db.js
+++ backend/Config/db.js
@@ -3,6 +3,6 @@ import mysql from "mysql2";
 export const db = mysql.createConnection({
     host:"localhost",
     user: "root",
-    password: "",
+    password: "Epf100406!",
     database: "worksy"
 });
